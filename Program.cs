caeac85a5f4b70b41d9b0c046d93f3b41e2b7922
anacarolinagit <76.anacarolina@gmail.com>
2024-06-01 09:48:23 -0300

    Desafio POO concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("+55(11)99999-9999", "Iphone 15", "000000-00-000000-0", 512);
+Nokia nokia = new Nokia("+55(11)99898-9898", "Nokia C100", "111111-11-111111-1", 32);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Apple Maps");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Google Maps");
